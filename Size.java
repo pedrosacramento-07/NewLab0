41a8e6bedbae75343e673bae748e66ea00586a1e
Pedro Sacramento <180221043@estudantes.ips.pt>
2021-03-04 11:54:54 +0000

    Documentação

--- Size.java
+++ Size.java
@@ -13,19 +13,27 @@ public enum Size {
     private final int minValue;
     private final int maxValue;
     private final char code;
-
+/**
+ * Construtor da classe
+ */
     private Size(int minValue, int maxValue, String description, char code){
         this.description=description;
         this.minValue=minValue;
         this.maxValue=maxValue;
         this.code=code;
     }
-
+/**
+ * Método toString 
+ * return o toString
+ */
     @Override
     public String toString() {
         return this.description;
     }
-
+/**
+ * Método que retorna o descrição
+ * return description- Descrição
+ */
     public String getDescription() {
         return description;
     }
