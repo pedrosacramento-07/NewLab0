466f23fdbd829d0bae8783043949ae21fd67ed86
Pedro Sacramento <180221043@estudantes.ips.pt>
2021-03-04 11:18:11 +0000

    Definição do tamanho das chavenas

---
+++ Size.java
@@ -0,0 +1,45 @@
+
+/**
+ * Enumeration class Size - write a description of the enum class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Size {
+    SMALL(32, 36, "Pequeno",'S'),
+    MEDIUM(37, 44, "Médio",'M'),
+    LARGE(45, 52, "Grande",'L');
+    private final String description;
+    private final int minValue;
+    private final int maxValue;
+    private final char code;
+
+    private Size(int minValue, int maxValue, String description, char code){
+        this.description=description;
+        this.minValue=minValue;
+        this.maxValue=maxValue;
+        this.code=code;
+    }
+
+    @Override
+    public String toString() {
+        return this.description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getMinValue() {
+        return minValue;
+    }
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public char getCode() {
+        return code;
+}
+
+}
